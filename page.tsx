5ff54bb9ed0b23446c5319059ef3fb004620a654
kahunaraihan01 <11407@smpn1godean.sch.id>
2026-04-16 20:31:26 +0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useState } from 'react';
+import DomeGallery from '@/components/DomeGallery';
+import InteractionFlow from '@/components/InteractionFlow';
+
+export default function Home() {
+  const [showGallery, setShowGallery] = useState(false);
+
+  const userImages = [
+    'Img 2jpeg.jpeg',
+    'Img 3.jpeg',
+    'Img 4.jpeg',
+    'Img 5.jpeg',
+    'Img 6.jpeg',
+    'Img 7.jpeg',
+    'Img 8.jpeg',
+
+  ];
+
+  return (
+    <main className="w-screen h-screen bg-[#060010]">
+      {!showGallery ? (
+        <InteractionFlow onFlowComplete={() => setShowGallery(true)} />
+      ) : (
+        <>
+          <audio src="/pretty.mp3" autoPlay loop className="hidden" />
+          <DomeGallery
+            images={userImages}
+            fit={0.8}
+            minRadius={600}
+            maxVerticalRotationDeg={0}
+            segments={34}
+            dragDampening={2}
+            grayscale={false}
+            autoRotationSpeed={0.1}
+          />
+        </>
+      )}
+    </main>
+  );
+}
